98ddabf7a54274563e55a901b7b12f7e57fc4184
Rohit <freelancerrohit46@gmail.com>
2025-06-17 14:49:43 +0530

    connected to mogo atlas

---
+++ src/db/connectDb.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDb = async ()=>{
+    try {
+        
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(`db connected seccessfull !! db Host: ${connectionInstance.connection.host}`);
+
+    } catch (error) {
+        console.error("Here is some error in connecting db ==> ",error);
+        throw error;
+    }
+}
+
+export default connectDb;
