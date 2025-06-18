df9d9b1e5bbb6f85d6f954b41fc76b4cd6946ab1
Rohit <freelancerrohit46@gmail.com>
2025-06-18 22:43:13 +0530

    some utils are added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import dotenv from 'dotenv';
 import connectDb from "./db/connectDb.js";
-
+import { app } from './app.js';
 
 dotenv.config({
     path: "./env"
 })
 
-connectDb();
+const port = process.env.PORT || 8000;
+
+connectDb()
+.then(()=>{
+    app.listen(port, () =>{
+        console.log(`server is listening on port ${port}`);
+        
+    })
+})
+.catch((error => {
+    console.log("Mongodb connection Error message from index.js file !! ",error);
+    
+}))
