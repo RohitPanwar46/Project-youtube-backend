7afe4bbf37bcf79ed79d65d7a5979169e01ee76f
Rohit <freelancerrohit46@gmail.com>
2025-08-03 12:50:19 +0530

    feat: add JWT import and enhance video upload functionality with error handling

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,6 +7,7 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {Video} from "../models/video.model.js"
 import { response } from "express"
+import jwt from 'jsonwebtoken'
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,7 +5,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import jwt from 'jsonwebtoken'
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
@@ -49,7 +49,37 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+
+    const videoFileLocalPath = await req.files?.videoFile?.[0]?.path;
+    const thumbnailLocalPath = await req.files?.thumbnail?.[0]?.path;
+
+    if (!videoFileLocalPath && !thumbnailLocalPath) {
+        throw new ApiError(401, "video and thumbnail is required")
+    }
+
+    let videoRef = await uploadOnCloudinary(videoFileLocalPath);
+    let thumbnailRef = await uploadOnCloudinary(thumbnailLocalPath);
+
+    if(!videoRef && !thumbnailRef){
+        throw new ApiError(500,"failed to uplaod video or thumbnail on cloudinary")
+    }
+
+    const incomingRefreshToken = await req.cookies.refreshToken || req.body.refreshToken;
+    const decodedToken = jwt.verify(incomingRefreshToken,process.env.ACCESS_TOKEN_SECRET);
+
+    const video = await Video.create({
+        videoFile: videoRef.url,
+        thumbnail: thumbnailRef.url,
+        title,
+        description,
+        duration: videoRef.duration,
+        owner: decodedToken._id
+    })
     
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video ,"video uploaded successfully"))
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
